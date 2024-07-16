acc589eb97ad93a03edde7091966f6b83119160f
Jiachen Li <alexjiachenli@hotmail.com>
2024-07-16 20:26:03 +1000

    try patch size 0.01

--- scripts/static.sh
+++ scripts/static.sh
@@ -1,5 +1,5 @@
 # N-MNIST
-python main.py --dataset mnist --polarity 1 --pos top-left --trigger_size 0.1 --epsilon 0.1 --type static --cupy --epochs 10
+python main.py --dataset mnist --polarity 1 --pos top-left --trigger_size 0.01 --epsilon 0.1 --type static --cupy --epochs 10
 
 # CIFAR-10
 python main.py --dataset cifar10 --polarity 1 --pos top-left --trigger_size 0.1 --epsilon 0.1 --type static --cupy --epochs 28
